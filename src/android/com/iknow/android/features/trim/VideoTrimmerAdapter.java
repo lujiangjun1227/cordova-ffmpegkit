7214c011712ba56219a8fec28072b47f7b80b13c
LuJiangjun <50285613+lujiangjun1227@users.noreply.github.com>
2024-06-03 15:55:58 +0800

    Update VideoTrimmerAdapter.java

--- src/android/com/iknow/android/features/trim/VideoTrimmerAdapter.java
+++ src/android/com/iknow/android/features/trim/VideoTrimmerAdapter.java
@@ -3,7 +3,7 @@ package com.iknow.android.features.trim;
 import android.content.Context;
 import android.graphics.Bitmap;
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
